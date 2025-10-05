9efc1ad120597f84db089f635ef8e6eb8696a94f
madhavi <madhavi.potru@gmail.com>
2025-10-05 21:18:56 +0530

    maven program

--- Add1.java
+++
@@ -1,9 +0,0 @@
-public class Add1{
-public static void main (String args []){
-
-
-System.out.println("hello");
-System.out.println("hello");
-
-}
-}
--- cat.java
+++
@@ -1,6 +0,0 @@
-class Cat {
-    public static void main String args[]
-    {     
-        System.out.printl()"helllo");
-    }
-}
